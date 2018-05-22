3df84f4653ecac5068bb94b92b895ac6317aa319
darwinjcas <35083261+darwinjcas@users.noreply.github.com>
2018-05-21 23:02:59 -0700

    add character + physics
    added character, jump script, and movement script

---
+++ Game/Assets/VaniaGame/Scripts/Jump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    public bool onGround;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        onGround = true;
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (onGround){
+            if (Input.GetButtonDown("Jump")) {
+
+                rb.velocity = new Vector3(0f, 4f, 0f);
+                onGround = false;
+            }
+        }
+		
+	}
+	private void OnCollisionEnter(Collision any)
+	{
+        any.gameObject.CompareTag("Ground");
+        onGround = true;
+	}
+}
---
+++ Game/Assets/VaniaGame/Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //create rigib body to add physics to the object
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+    //always initialize a variable to the rigid body    
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * 5;
+        float v = Input.GetAxis("Vertical") * 5;
+
+        Vector3 vel = rb.velocity;
+        vel.x = -h;
+        vel.z = -v;
+        rb.velocity = vel;
+
+
+
+    }
+}
